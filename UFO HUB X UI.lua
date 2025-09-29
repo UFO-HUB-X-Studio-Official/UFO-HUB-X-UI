83a03af42bcbbd1b2eb6ef9b01a1cfad9b577768
UFO-HUB-X-Studio-Official <ufohubxstudio@gmail.com>
2025-09-29 21:04:09 +0700

    Update UFO HUB X UI.lua

--- UFO HUB X UI.lua
+++ UFO HUB X UI.lua
@@ -116,8 +116,8 @@ end
 
 -- ===== UFO + TITLE (ปรับตามคำขอ) =====
 do
-    local UFO_Y_OFFSET   = 70  -- ⬇️ ยานลงมาใกล้กรอบ
-    local TITLE_Y_OFFSET = 5  -- ⬆️ ชื่อขึ้นไปอีกนิด
+    local UFO_Y_OFFSET   = 60  -- ⬇️ ยานลงมาใกล้กรอบ
+    local TITLE_Y_OFFSET = 8  -- ⬆️ ชื่อขึ้นไปอีกนิด
 
     -- UFO
     local UFO = Instance.new("ImageLabel", Window)
